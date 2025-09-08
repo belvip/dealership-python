469b62392a559b0b3c911a46ef3b9d444910b658
belvip <pouadjeubelvi@gmail.com>
2025-09-08 18:30:43 +0000

    Create Django views to get dealers

--- djangoapp/restapis.py
+++ djangoapp/restapis.py
@@ -9,7 +9,7 @@ backend_url = os.getenv(
     'backend_url', default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    default="http://localhost:5000/")
 
 # def get_request(endpoint, **kwargs):
 
@@ -31,9 +31,15 @@ def get_request(endpoint, **kwargs):
         print("Network exception occurred")
 # Add code for get requests to back end
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
-# Add code for retrieving sentiments
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
 
 # def post_review(data_dict):
 # Add code for posting review
